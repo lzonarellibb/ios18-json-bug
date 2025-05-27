4e843458d99e3602db2331fca3c64c23d6cc5f1f
lzonarellibb <lzonarelli@bitbang.com>
2025-05-27 10:40:26 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 async function getData() {
-  const url = "https://example.org/products.json";
+  const url = "https://github.com/lzonarellibb/ios18-json-bug/blob/main/data.json";
   try {
     const response = await fetch(url);
     if (!response.ok) {
